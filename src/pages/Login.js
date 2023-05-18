60f9c2e6be797c7ccae5e05457811fe5b4c86cbe
santosh-chaugule <santoshchaugule1697@gmail.com>
2023-05-18 15:15:07 +0530

    OK

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+
+//Import Area
+import React from 'react'
+
+
+//Defination area
+export default function Login() {
+    return (
+        <div>Login</div>
+    )
+}
+//Export area
---
+++ src/pages/NotFound.js
@@ -0,0 +1,18 @@
+
+//import area
+import React from 'react'
+
+//defination area
+export default function NotFound() {
+    return (
+        <>
+            <div>NotFound</div>
+            <a href='/'>Go to Home page</a>
+
+        </>
+
+
+    )
+}
+
+//export area
